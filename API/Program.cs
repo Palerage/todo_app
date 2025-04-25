d4f22838d9764cbbdbc517e81d3facd129c55095
Palerage <royne@outlook.com>
2025-04-25 10:52:15 +0200

    minor update, mic check

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,6 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
 
